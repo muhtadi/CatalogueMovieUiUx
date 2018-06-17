3dfa49b8a7604982edc306df376c89a38301917c
Muhtadi Irfan <muhtadi.irfan@gmail.com>
2018-06-17 20:38:25 +0700

    adding detail activity

---
+++ app/src/main/java/com/example/muhtadi/cataloguemovieuiux/Adapter/RecyclerViewItemClickListener.java
@@ -0,0 +1,7 @@
+package com.example.muhtadi.cataloguemovieuiux.Adapter;
+
+import android.view.View;
+
+public interface RecyclerViewItemClickListener {
+    void onItemClick(int position, View view);
+}
